dcf22a151aedb04600469e247242f811889fecd6
LAPTOP-M\81808 <miki.snoopy.729@gmail.com>
2024-10-16 22:47:03 +0900

    JavaBronzeSE Chapter2 Sample2_3～Sample2_5まで対応（変数や定数の宣言と初期化、配列）

---
+++ JavaStudy/src/bronze/purple/chapter2/Sample2_3.java
@@ -0,0 +1,10 @@
+package bronze.purple.chapter2;
+
+public class Sample2_3 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        long num2 = 30;
+        long num3 = 1000000000;
+        float num4 = 10.0F;
+    }
+}
---
+++ JavaStudy/src/bronze/purple/chapter2/Sample2_4.java
@@ -0,0 +1,10 @@
+package bronze.purple.chapter2;
+
+public class Sample2_4 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        num1 = 20;
+        final int num2 = 10;
+//        num2 = 20;
+    }
+}
